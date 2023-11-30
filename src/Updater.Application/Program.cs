1041b482511c7fbd73754d7141ae29515dce2403
mjamesharmon <m.james.harmon@fastmail.com>
2023-11-29 22:28:58 -0800

    added image header

--- src/Updater.Application/Program.cs
+++ src/Updater.Application/Program.cs
@@ -4,6 +4,9 @@ using Updater.Core.Extensions;
 
 await Profile.Configure(options =>
    options.UseHttp()).
+   AddImage(
+    "https://github.com/mjamesharmon/mjamesharmon/blob/main/assets/img/hello.jpg?raw=true",
+    "Hello").
    AddHeader("Welcome", "I started coding when I was around 10 years old " +
      "and I'm still not bored with it.For December, check back to follow" +
      " the countdown calendar for seasonal vids and of course be sure to " +
