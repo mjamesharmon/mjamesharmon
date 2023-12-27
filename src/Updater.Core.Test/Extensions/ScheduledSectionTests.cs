af1d830b02dfe29d8b32e619de8b4c37c1cbddb8
mjamesharmon <m.james.harmon@fastmail.com>
2023-12-26 23:59:58 -0800

    refactoring unit tests to use test data classes

--- src/Updater.Core.Test/Extensions/ScheduledSectionTests.cs
+++ src/Updater.Core.Test/Extensions/ScheduledSectionTests.cs
@@ -5,6 +5,7 @@ namespace Updater.Core.Test.Extensions
 {
     public class ScheduledSectionTests
     {
+        private const string SectionData = "hello world";
 
         [Fact]
         public void TestRepeatYearly_NotConfigured_ThrowsException()
@@ -15,7 +16,7 @@ namespace Updater.Core.Test.Extensions
                 profile.RepeatYearly(DateTime.MinValue,
                 DateTime.MinValue.AddDays(4),
                 p => p.
-                    AddSection("hello world")));
+                    AddSection(SectionData)));
         }
 
 
@@ -26,7 +27,7 @@ namespace Updater.Core.Test.Extensions
 
             Assert.Throws<InvalidOperationException>(() =>
                 profile.IncludeAfter(DateTime.MinValue, p => p.
-                    AddSection("hello world")));
+                    AddSection(SectionData)));
         }
 
         [Fact]
@@ -37,7 +38,7 @@ namespace Updater.Core.Test.Extensions
 
             Assert.Throws<InvalidOperationException>(() =>
                 profile.RemoveAfter(DateTime.MinValue, p => p.
-                    AddSection("hello world")));
+                    AddSection(SectionData)));
         }
 
         [Fact]
@@ -48,7 +49,51 @@ namespace Updater.Core.Test.Extensions
 
             Assert.Throws<InvalidOperationException>(() =>
                 profile.ScheduleFor(DateTime.MinValue, DateTime.MaxValue,
-                p => p.AddSection("hello world")));
+                p => p.AddSection(SectionData)));
+        }
+
+        [Theory]
+        [ClassData(typeof(IncludeAfterTestData))]
+        public void TestIncludeAfter_Valid_Ok(DateTime now, DateTime start,
+            bool isValid) {
+
+            var profile = Profile.Configure(options => options.
+                EnableScheduling(now)).
+                IncludeAfter(start, p => p.AddSection(SectionData)).
+                Build();
+
+            Assert.NotNull(profile);
+            Assert.Equal(isValid, profile.Contains(SectionData));     
+        }
+
+        [Theory]
+        [ClassData(typeof(RemoveAfterTestData))]
+        public void TestRemoveAfter_Valid_Ok(DateTime now, DateTime start,
+            bool isValid)
+        {
+
+            var profile = Profile.Configure(options => options.
+                EnableScheduling(now)).
+                RemoveAfter(start, p => p.AddSection(SectionData)).
+                Build();
+
+            Assert.NotNull(profile);
+            Assert.Equal(isValid, profile.Contains(SectionData));
+        }
+
+        [Theory]
+        [ClassData(typeof(ScheduleForTestData))]
+        public void TestScheduleFor_Valid_Ok(DateTime now, DateTime start,
+            DateTime end, bool isValid)
+        {
+
+            var profile = Profile.Configure(options => options.
+                EnableScheduling(now)).
+                ScheduleFor(start, end, p => p.AddSection(SectionData)).
+                Build();
+
+            Assert.NotNull(profile);
+            Assert.Equal(isValid, profile.Contains(SectionData));
         }
     }
 }
