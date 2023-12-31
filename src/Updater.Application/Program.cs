e25905596fd96f584b983fe87cfccdb84f7b49e9
M. James Harmon <mjamesharmon@users.noreply.github.com>
2023-12-30 23:23:21 -0800

    Update Program.cs

--- src/Updater.Application/Program.cs
+++ src/Updater.Application/Program.cs
@@ -7,12 +7,6 @@ await Profile.Configure(options => options.
    UseHttp().
    EnableScheduling()).
    AddImage(ProfileSettings.HeaderImage, ProfileSettings.HeaderImageAlt).
-   AddHeader("Welcome", "I started coding when I was around 10 years old " +
-     "and I'm still not bored with it. For December, check back to follow" +
-     " the countdown calendar for seasonal vids and of course be sure to " +
-     "keep" +
-     " up with the Wham Watchdog report to see when " +
-     "'Last Christmas' tops the charts :)").
      RepeatYearly(ProfileSettings.ChristmasContentStart,
        ProfileSettings.ChristmasContentEnd, profile => profile.
         AddSection("Countdown Calendar").
