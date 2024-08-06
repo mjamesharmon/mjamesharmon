6b217fc959a222caba34207306423cd471a1a863
mjamesharmon <m.james.harmon@fastmail.com>
2024-08-05 23:22:43 -0700

    reducing to most recent 3 posts

--- src/Updater.Application/Program.cs
+++ src/Updater.Application/Program.cs
@@ -12,7 +12,7 @@ await Profile.Configure(options => options.
 
    // primary content
    AddImage(ProfileSettings.HeaderImage, ProfileSettings.HeaderImageAlt).
-   AddLatestFromDevTo(4,ProfileSettings.DevToProfile).
+   AddLatestFromDevTo(3,ProfileSettings.DevToProfile).
 
    // christmas content section
    RepeatYearly(ProfileSettings.ChristmasContentStart,
